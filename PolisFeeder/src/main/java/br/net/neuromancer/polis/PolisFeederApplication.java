181adfaed68820bd32ea753ad270e29f4d8dd148
Marco Antonio Almeida Silva <marco.antonio.almeida.silva@gmail.com>
2015-11-08 20:50:32 -0200

    initial seed commit

---
+++ PolisFeeder/src/main/java/br/net/neuromancer/polis/PolisFeederApplication.java
@@ -0,0 +1,12 @@
+package br.net.neuromancer.polis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolisFeederApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolisFeederApplication.class, args);
+    }
+}
---
+++ PolisFeeder/src/test/java/br/net/neuromancer/polis/PolisFeederApplicationTests.java
@@ -0,0 +1,16 @@
+package br.net.neuromancer.polis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PolisFeederApplication.class)
+public class PolisFeederApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
